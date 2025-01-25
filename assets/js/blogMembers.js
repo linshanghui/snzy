81fc468b9315ef2087f1fb959f945c1885fb9402
阿慧 <240233869@qq.com>
2025-01-25 03:41:14 +0000

    gx
    Signed-off-by: 阿慧 <240233869@qq.com>

---
+++ assets/js/blogMembers.js
@@ -0,0 +1,10 @@
+// blogMembers.js
+const blogMembers = [
+    { id: "1", name: "博客1", url: "https://example1.com", joinTime: "2022-01-01", message: "欢迎来到我的博客！" },
+    { id: "2", name: "博客2", url: "https://example2.com", joinTime: "2022-02-15", message: "一起走过十年！" },
+    { id: "3", name: "博客3", url: "https://example3.com", joinTime: "2022-03-20", message: "十年之约，不见不散！" },
+    { id: "4", name: "博客4", url: "https://example4.com", joinTime: "2022-04-10", message: "希望我的博客能给你带来灵感！" },
+    { id: "5", name: "博客5", url: "https://example5.com", joinTime: "2022-05-05", message: "期待与你一起成长！" }
+];
+
+export default blogMembers;
